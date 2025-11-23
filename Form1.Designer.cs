c354e447820b865f8efd50168bd2356fc08de2c5
Youssef_Saied <108391666+Yousefsaied4911@users.noreply.github.com>
2025-11-23 09:00:28 +0200

    .

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbProgressPresent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRedo = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnRemoveTask = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClearALL = new System.Windows.Forms.Button();
+            this.editTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("Myanmar Text", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.ImageKey = "Task.png";
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.treeView1.LabelEdit = true;
+            this.treeView1.Location = new System.Drawing.Point(263, 101);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(244, 291);
+            this.treeView1.TabIndex = 19;
+            this.treeView1.BeforeCheck += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCheck);
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "UnCheked.png");
+            this.imageList1.Images.SetKeyName(1, "Cheked.png");
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(31, 261);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(220, 24);
+            this.txtTask.TabIndex = 18;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(543, 261);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 33);
+            this.progressBar1.TabIndex = 17;
+            // 
+            // lbProgressPresent
+            // 
+            this.lbProgressPresent.AutoSize = true;
+            this.lbProgressPresent.BackColor = System.Drawing.Color.White;
+            this.lbProgressPresent.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProgressPresent.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbProgressPresent.Location = new System.Drawing.Point(626, 314);
+            this.lbProgressPresent.Name = "lbProgressPresent";
+            this.lbProgressPresent.Size = new System.Drawing.Size(61, 40);
+            this.lbProgressPresent.TabIndex = 16;
+            this.lbProgressPresent.Text = "0%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(570, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 40);
+            this.label2.TabIndex = 15;
+            this.label2.Text = " Progress ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(265, -9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 52);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "To Do List";
+            // 
+            // btnRedo
+            // 
+            this.btnRedo.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnRedo.BackgroundImage = global::ToDoList.Properties.Resources.RedoDisabled;
+            this.btnRedo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRedo.Location = new System.Drawing.Point(299, 62);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(30, 33);
+            this.btnRedo.TabIndex = 13;
+            this.btnRedo.UseVisualStyleBackColor = false;
+            this.btnRedo.Click += new System.EventHandler(this.btnRedo_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnUndo.BackgroundImage = global::ToDoList.Properties.Resources.UndoDisabled;
+            this.btnUndo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUndo.Location = new System.Drawing.Point(263, 62);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(30, 33);
+            this.btnUndo.TabIndex = 12;
+            this.btnUndo.UseVisualStyleBackColor = false;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnRemoveTask
+            // 
+            this.btnRemoveTask.Font = new System.Drawing.Font("MV Boli", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveTask.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnRemoveTask.Location = new System.Drawing.Point(73, 386);
+            this.btnRemoveTask.Name = "btnRemoveTask";
+            this.btnRemoveTask.Size = new System.Drawing.Size(137, 49);
+            this.btnRemoveTask.TabIndex = 11;
+            this.btnRemoveTask.Text = "Remove Task";
+            this.btnRemoveTask.UseVisualStyleBackColor = true;
+            this.btnRemoveTask.Click += new System.EventHandler(this.btnRemoveTask_Click);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Font = new System.Drawing.Font("MV Boli", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTask.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnAddTask.Location = new System.Drawing.Point(73, 305);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(137, 49);
+            this.btnAddTask.TabIndex = 10;
+            this.btnAddTask.Text = "Add Task ";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.editTaskToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 52);
+            this.contextMenuStrip1.Text = "File";
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
+            this.deleteToolStripMenuItem.Text = "Delete Task";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // btnClearALL
+            // 
+            this.btnClearALL.Font = new System.Drawing.Font("MV Boli", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearALL.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnClearALL.Location = new System.Drawing.Point(73, 463);
+            this.btnClearALL.Name = "btnClearALL";
+            this.btnClearALL.Size = new System.Drawing.Size(137, 49);
+            this.btnClearALL.TabIndex = 20;
+            this.btnClearALL.Text = "Clear All";
+            this.btnClearALL.UseVisualStyleBackColor = true;
+            this.btnClearALL.Click += new System.EventHandler(this.btnClearALL_Click);
+            // 
+            // editTaskToolStripMenuItem
+            // 
+            this.editTaskToolStripMenuItem.Name = "editTaskToolStripMenuItem";
+            this.editTaskToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.editTaskToolStripMenuItem.Text = "Edit Task";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.btnClearALL);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lbProgressPresent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRedo);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnRemoveTask);
+            this.Controls.Add(this.btnAddTask);
+            this.Name = "Form1";
+            this.Text = "To do list";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbProgressPresent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRedo;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnRemoveTask;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.Button btnClearALL;
+        private System.Windows.Forms.ToolStripMenuItem editTaskToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using ToDoList.Properties;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stack<System.Windows.Forms.TreeNode> _Undo = new Stack<System.Windows.Forms.TreeNode>();
+        private Stack<System.Windows.Forms.TreeNode> _Redo = new Stack<System.Windows.Forms.TreeNode>();
+
+   
+
+        System.Windows.Forms.TreeNode CopyOfTreeNode()
+        {
+            System.Windows.Forms.TreeNode CopyTreeNode = new System.Windows.Forms.TreeNode();
+            foreach (TreeNode Node in treeView1.Nodes)
+            {
+                CopyTreeNode.Nodes.Add((TreeNode)Node.Clone());
+            }
+
+            return CopyTreeNode;
+        }
+
+        void UpdateTreeViewOfForm(System.Windows.Forms.TreeNode NewTreeViewNode)
+        {
+            treeView1.Nodes.Clear();
+            foreach (TreeNode Node in NewTreeViewNode.Nodes)
+            {
+                treeView1.Nodes.Add((TreeNode)Node.Clone());
+            }
+
+            treeView1.Refresh();
+        }
+
+        void UndoPush()
+        {
+            _Undo.Push(CopyOfTreeNode());
+            btnUndo.Enabled = true;
+            btnUndo.BackgroundImage = Resources.UndoEnabled;
+        }
+
+        void RedoPush()
+        {
+            _Redo.Push(CopyOfTreeNode());
+            btnRedo.Enabled = true;
+            btnRedo.BackgroundImage = Resources.RedoEnabled;
+        }
+
+        private bool ValidateTaskInput()
+        {
+            if (txtTask.Text.Trim() == "")
+            {
+                MessageBox.Show("Task Cannot Be Empty!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        void AddTask()
+        {
+            UndoPush();
+
+            TreeNode Node = new TreeNode(txtTask.Text);
+            Node.ContextMenuStrip = contextMenuStrip1;
+            treeView1.Nodes.Add(Node);
+            CalculateProgressSteps();
+        }
+
+        void DeleteTask()
+        {
+            UndoPush();
+
+            if (treeView1.SelectedNode != null)
+            {
+                treeView1.Nodes.Remove(treeView1.SelectedNode);
+                CalculateProgressSteps();
+                CalculateProgressBar();
+            }
+        }
+
+        void EditTask()
+        {
+            if (treeView1.SelectedNode != null)
+            {
+                UndoPush();
+                treeView1.SelectedNode.BeginEdit();
+            }
+        }
+
+        void RemoveTask()
+        {
+            UndoPush();
+            if (treeView1.Nodes.Count != 0)
+            {
+                treeView1.Nodes.RemoveAt(treeView1.Nodes.Count - 1);
+                CalculateProgressSteps();
+                CalculateProgressBar();
+            }
+        }
+
+        void ClearAllTaske()
+        {
+            if (treeView1.Nodes.Count == 0)
+                return;
+
+            UndoPush();
+            treeView1.Nodes.Clear();
+            CalculateProgressSteps();
+            CalculateProgressBar();
+        }
+
+        void ConfirmClearAllTasks()
+        {
+            var result = MessageBox.Show("Are you sure you want to clear all tasks?", "Confirm Clear All", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                ClearAllTaske();
+            }
+        }
+
+        private void ChageTaskIconAfterCheck(TreeNode node)
+        {
+            node.ImageIndex = node.Checked ? 1 : 0;
+            node.SelectedImageIndex = node.Checked ? 1 : 0;
+        }
+
+    
+
+        void CalculateProgressSteps()
+        {
+            if (treeView1.Nodes.Count == 0)
+            {
+                progressBar1.Step = 0;
+                return;
+            }
+
+            double Steps = (double)100 / treeView1.Nodes.Count;
+            progressBar1.Step = (int)Math.Ceiling(Steps);
+        }
+
+        void CalculateProgressBar()
+        {
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+
+            foreach (TreeNode Node in treeView1.Nodes)
+            {
+                if (Node.Checked)
+                {
+                    progressBar1.PerformStep();
+                }
+            }
+
+            if (progressBar1.Maximum > 0)
+            {
+                lbProgressPresent.Text = (((float)progressBar1.Value / progressBar1.Maximum) * 100 + "%");
+            }
+            else
+            {
+                lbProgressPresent.Text = "0%";
+            }
+        }
+
+        
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (!ValidateTaskInput())
+            {
+                return;
+            }
+
+            AddTask();
+            txtTask.Text = "";
+            CalculateProgressSteps();
+            CalculateProgressBar();
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            RemoveTask();
+            CalculateProgressSteps();
+            CalculateProgressBar();
+        }
+
+        private void btnClearALL_Click(object sender, EventArgs e)
+        {
+            ConfirmClearAllTasks();
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (_Undo.Count > 0)
+            {
+                RedoPush();
+                UpdateTreeViewOfForm(_Undo.Peek());
+                _Undo.Pop();
+            }
+
+            if (_Undo.Count == 0)
+            {
+                btnUndo.Enabled = false;
+                btnUndo.BackgroundImage = Resources.UndoDisabled;
+            }
+
+            CalculateProgressSteps();
+            CalculateProgressBar();
+        }
+
+        private void btnRedo_Click(object sender, EventArgs e)
+        {
+            if (_Redo.Count > 0)
+            {
+                UndoPush();
+                UpdateTreeViewOfForm(_Redo.Peek());
+                _Redo.Pop();
+            }
+
+            if (_Redo.Count == 0)
+            {
+                btnRedo.Enabled = false;
+                btnRedo.BackgroundImage = Resources.RedoDisabled;
+            }
+
+            CalculateProgressSteps();
+            CalculateProgressBar();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+        }
+
+        private void treeView1_BeforeCheck(object sender, TreeViewCancelEventArgs e)
+        {
+            UndoPush();
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            ChageTaskIconAfterCheck(e.Node);
+            CalculateProgressBar();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Text)
+            {
+                case "Delete Task":
+                    DeleteTask();
+                    break;
+                case "Edit Task":
+                    EditTask();
+                    break;
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+        }
+    }
+}
